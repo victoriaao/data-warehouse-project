debfa2b54ad226b6247b92e341dab16776ffda9c
Victoria A Olorunfemi <90652420+victoriaao@users.noreply.github.com>
2025-07-10 01:28:33 +0100

    Update load_silver.sql

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -40,8 +40,9 @@ SELECT
     cst_create_date
 FROM flag_ref;
 
+
 -- Loading crm_sales_details
-TRUNCATE TABLE silver.crm_sales_details
+TRUNCATE TABLE silver.crm_sales_details;
 INSERT INTO silver.crm_sales_details (sls_ord_num,sls_prd_key,sls_cust_id,sls_order_dt,sls_ship_dt,sls_due_dt,sls_sales,sls_quantity,sls_price)
 SELECT
 sls_ord_num,
@@ -66,4 +67,64 @@ CASE WHEN sls_price IS NULL OR sls_price <= 0
     THEN ROUND(sls_sales/ NULLIF(sls_quantity,0))
     ELSE ROUND(sls_price)
 END AS sls_price
-FROM bronze.crm_sales_details
+FROM bronze.crm_sales_details;
+
+
+-- Loading crm_prd_info
+TRUNCATE TABLE silver.crm_prd_info;
+INSERT INTO silver.crm_prd_info (prd_id, cat_id, prd_key, prd_nm, prd_cost, prd_line, prd_start_dt, prd_end_dt )
+SELECT 
+prd_id,
+REPLACE(SUBSTR(prd_key,1,5), '-','_') AS cat_id, -- derive new category ID column by extracting
+SUBSTR(prd_key,7, LENGTH(prd_key)) AS prd_key, -- derive new product Key column by extracting
+prd_nm,
+IFNULL (prd_cost,0) as prd_cost,
+CASE UPPER(TRIM(prd_line)) 
+    WHEN 'M' THEN 'Mountain'
+    WHEN 'R' THEN 'Road'
+    WHEN 'S' THEN 'Other Sales'
+    WHEN 'T' THEN 'Touring'
+    ELSE 'Unknown'
+    END AS prd_line,
+prd_start_dt,
+LEAD (prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) -1 AS prd_end_dt -- calculate end date as one day before the next start date
+FROM bronze.crm_prd_info;
+
+-- Loading erp_cust_az12
+TRUNCATE TABLE silver.erp_cust_az12;
+INSERT INTO silver.erp_cust_az12 (cid,bdate,gen)
+SELECT 
+CASE WHEN cid LIKE 'NAS%'
+    THEN SUBSTR(cid,4,LENGTH(cid))
+    ELSE cid
+END AS cid,
+CASE WHEN bdate > CURRENT_DATE THEN NULL
+    ELSE bdate
+END AS bdate,
+CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+     WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+     ELSE 'Unknown'
+END AS gen
+FROM bronze.erp_cust_az12;
+
+-- Loading erp_loc_a101
+TRUNCATE TABLE silver.erp_loc_a101;
+INSERT INTO silver.erp_loc_a101 (cid,cntry)
+SELECT
+REPLACE(cid,'-','') cid,
+CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+     WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+     WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'Unknown'
+ELSE TRIM(cntry)
+END AS cntry
+FROM bronze.erp_loc_a101;
+
+-- Loading erp_px_cat_g1v2
+TRUNCATE TABLE silver.erp_px_cat_g1v2;
+INSERT INTO silver.erp_px_cat_g1v2 (id,cat,subcat,maintenance)
+SELECT
+id,
+cat,
+subcat,
+maintenance
+FROM bronze.erp_px_cat_g1v2;
